4b81f92851a8119431099e1e77abe9ac15ab8575
Lale <Lale11Ortiz@gmail.com>
2025-05-30 22:32:45 -0300

    Corrijo estilo de código según flake8

--- Automation/test_ci_cd_ok.py
+++ Automation/test_ci_cd_ok.py
@@ -1,10 +1,12 @@
-# Prueba flake8 limpio
 import unittest
 
+
 class BasicCICDTest(unittest.TestCase):
+
     def test_ci_cd_pipeline(self):
         print("✅ GitHub Actions funciona correctamente con unittest.")
         self.assertTrue(True)
 
+        
 if __name__ == "__main__":
     unittest.main()
